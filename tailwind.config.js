1494e64fd9ecdd9092189df8d06c13b8b4f68a34
Anne <anneris.wong@gmail.com>
2023-09-27 12:49:53 -0400

    Updating tailwing template with custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,19 @@ module.exports = {
       fontFamily: {
         ubuntu: ["Ubuntu", defaultTheme.fontFamily.sans],
       },
+      colors: {
+        "silver-bg": "#EFF5FF",
+        denim: "#022959",
+        grey: "#9699AA",
+        "light-blue": "#ABBCFF",
+        "light-grey": "#D6D9E6",
+        orange: "#FFAF7E",
+        pink: "#F9818E",
+        purple: "#483EFF",
+        "red-errors": "#EE374A",
+        "sky-blue": "#BEE2FD",
+        "very-light-grey": "#F8F9FF",
+      },
     },
   },
   variants: {
